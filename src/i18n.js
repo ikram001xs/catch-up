03230d0d4a630d3b8749d7d36dc7ba9e7dec8bbb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-20 11:25:20 -0500

    feat(i18n): enable composition api by setting legacy to false.

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,7 @@ import es from './locales/es.json';
 import { createI18n } from 'vue-i18n';
 
 const i18n = createI18n({
+    legacy: false,
     locale: 'en',
     fallbackLocale: 'en',
     messages: { en, es }
